ca3e477d3587086d4ea77738154884bed1493f28
Nikitarg1 <nikita.gomozov@gmail.com>
2020-11-05 19:26:54 +0300

    color change

--- app/src/main/java/MyProgram.java
+++ app/src/main/java/MyProgram.java
@@ -4,8 +4,7 @@ public class MyProgram {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        long a = in.nextInt();
-
-        System.out.println((a & -a) == a ? '0' : '1');
+        int a = in.nextInt();
+        System.out.println(a);
     }
 }
