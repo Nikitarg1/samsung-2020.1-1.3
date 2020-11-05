298f76fa2534ce6e67785f726831f83e4de0b93e
Nikitarg1 <nikita.gomozov@gmail.com>
2020-11-05 17:16:39 +0300

    color change

--- app/src/main/java/MyProgram.java
+++ app/src/main/java/MyProgram.java
@@ -1,6 +1,11 @@
-public class MyProgram{
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
+import java.util.Scanner;
+
+public class MyProgram {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        long a = in.nextInt();
+
+        System.out.println((a & -a) == a ? '0' : '1');
+    }
 }
- 
